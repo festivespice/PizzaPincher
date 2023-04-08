30b8ebbc686117f3ca20c1e026dc482f02ba12dc
ThePineapplePizza1 <43504303+ThePineapplePizza1@users.noreply.github.com>
2023-04-08 17:06:54 -0400

    added initial implementation of Google Places API
    added axios to package.json and package-lock.json as required for the API.
    Example code from Google's documentation for nearby search was added and tested working.

--- pizza-pincher-server/src/controllers/GooglePlaces.ts
+++ pizza-pincher-server/src/controllers/GooglePlaces.ts
@@ -1,1 +1,27 @@
-//todo
+//npx ts-node .\src\controllers\GooglePlaces.ts to run this
+
+import dotenv from 'dotenv';
+
+dotenv.config(); //loads environment variables hidden from common view.
+
+var api = process.env.API_KEY;
+// need to split up this url more to for parameters
+var fullUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=cruise&key=" + api
+
+// Googles code for using nearby searches with Places API
+// https://developers.google.com/maps/documentation/places/web-service/search-nearby#maps_http_places_nearbysearch-js
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: fullUrl,
+  headers: { }
+};
+
+axios(config)
+.then(function (response: any) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error: any) {
+  console.log(error);
+});
