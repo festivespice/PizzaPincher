ac8ed4d9131ec7f1beceed7dce85c8d776182e09
ThePineapplePizza1 <43504303+ThePineapplePizza1@users.noreply.github.com>
2023-04-18 22:24:15 -0400

    implemented receiving JSON of address and pizza info
    added response statuses
    address received is sent to Google
    pizza info parsed but not used

--- pizza-pincher-server/src/controllers/GooglePlaces.ts
+++ pizza-pincher-server/src/controllers/GooglePlaces.ts
@@ -7,13 +7,36 @@ const api = process.env.API_KEY;
 
 const axios = require('axios');
 
-export async function getNearbyPlaces() {
+export async function getNearbyPlaces(pizzaInfoAddress : JSON) {
+  console.log(pizzaInfoAddress);
+
+  // fix type error, pizzaInfoAddress[anything] gives error
+  let pizzaInfoAddressJSON = JSON.parse(JSON.stringify(pizzaInfoAddress));
+
+  // pizza will either only have a single value or have cheese, sauce, size, and toppings nested in it
+  if (!pizzaInfoAddressJSON["pizza"].hasOwnProperty("cheese")) {
+    var pizza = pizzaInfoAddressJSON["pizza"];
+    console.log(pizza);
+  } else {
+    var cheese = pizzaInfoAddressJSON["pizza"]["cheese"];
+    var sauce = pizzaInfoAddressJSON["pizza"]["sauce"];
+    var size = pizzaInfoAddressJSON["pizza"]["size"];
+    var toppings = pizzaInfoAddressJSON["pizza"]["toppings"];
+    console.log(cheese, sauce, size, toppings);
+  }
+  // will send this information somewhere later
+  if (typeof pizza !== "undefined") {
+    // TODO send pizza info
+  } else {
+    // TODO send cheese sauce size toppings
+  }
+
   // get the latitude and longitude
-  let latLng  = await getLatLng();
+  let latLng  = await getLatLng(pizzaInfoAddressJSON["place"].replaceAll(" ", "+"));
   let lat = latLng["results"][0]["geometry"]["location"]["lat"];
   let lng = latLng["results"][0]["geometry"]["location"]["lng"];
 
-  let radius = "500"; // radius to search in meters
+  let radius = "8000"; // radius to search in meters, 8000 meters is about 5 miles
 
   let fullUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "%2C" + lng +
       "&radius=" + radius + "&type=restaurant&keyword=pizza&key=" + api
@@ -26,7 +49,7 @@ export async function getNearbyPlaces() {
 
   return axios(config)
   .then(function (response: AxiosResponse<any>) {
-    return JSON.stringify(response.data); // temporarily returned as string until target information identified
+    return response.data; // temporarily returned as string until target information identified
   })
   .catch(function (error: AxiosError<any>) {
     console.log(error);
@@ -34,10 +57,9 @@ export async function getNearbyPlaces() {
   });
 }
 
-function getLatLng() {
+function getLatLng(address : string) {
 
   // address example format 1600+Amphitheatre+Parkway,+Mountain+View,+CA
-  let address = "Lakeland+Square+Mall,+Lakeland,+FL"
   let fullUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + api
 
 
@@ -50,7 +72,6 @@ function getLatLng() {
   return axios(config)
   .then(function (response : AxiosResponse<any>) {
     return response.data
-
   })
   .catch(function (error : AxiosError<any>) {
     console.log(error);
--- pizza-pincher-server/src/routes/GooglePlaces.ts
+++ pizza-pincher-server/src/routes/GooglePlaces.ts
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser')
 
 const googleController = require('../controllers/GooglePlaces')
 
 // get for Google Places
-router.get("/yummyPlaces", async function (req : any, res : any) {
-  // send string of nearby pizza places
-  res.send(await googleController.getNearbyPlaces());
+router.get("/yummyPlaces", bodyParser.json(), async function (req : any, res : any) {
+  // check for empty body
+  if (JSON.stringify(req.body) == "{}") {
+    res.status(400)
+    res.send("Empty body")
+    return
+  }
+
+  // send nearby places response from google using req body json
+  res.status(200)
+  res.send(await googleController.getNearbyPlaces(req.body));
+
 })
 
 module.exports = router;
